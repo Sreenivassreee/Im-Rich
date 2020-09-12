651b291757e82cd782146ff6f8f003ec144fced8
Sreenivas <ksreenivascse@gmail.com>
2020-09-12 20:08:02 +0530

    Initial Commit

---
+++ Im Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Im Rich
+//
+//  Created by Sreenivas K on 12/09/20.
+//  Copyright © 2020 Sreenivas K. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
